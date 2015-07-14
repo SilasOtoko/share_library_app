df26e251a14285dab863e763f6898ac96927f349
Josiah Schaefer <penname.silas@gmail.com>
2015-07-14 17:30:03 -0400

    fixed requiring user when registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, except: [:show]
+  before_action :require_user, except: [:show, :new]
 
   def show
 
