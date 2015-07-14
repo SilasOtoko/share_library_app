b460d3488b1891f7482271b158ef2e8b80963cfa
Josiah Schaefer <penname.silas@gmail.com>
2015-07-14 17:34:28 -0400

    Added require same user to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, except: [:show, :new]
+  before_action :require_same_user, only: [:edit, :update]
 
   def show
 
@@ -42,5 +42,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find_by slug: params[:id]
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You are not allowed to do that"
+      redirect_to root_path
+    end
   end
 end
