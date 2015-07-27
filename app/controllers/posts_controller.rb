da5ecee500ba1475c6585e54ac5e05591ffd0a34
Josiah Schaefer <penname.silas@gmail.com>
2015-07-27 18:06:59 -0400

    ordered posts in descending order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,9 @@ class PostsController < ApplicationController
 
   def index
     if logged_in?
-      @posts = Post.limit(Post::PER_PAGE).offset(params[:offset])
+      @posts = Post.limit(Post::PER_PAGE).offset(params[:offset]).order(created_at: :desc)
       @pages = (Post.all.size.to_f / Post::PER_PAGE).ceil
-      @posts_this_month = Post.limit(Post::PER_PAGE).offset(params[:offset]).where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight)
+      @posts_this_month = Post.limit(Post::PER_PAGE).offset(params[:offset]).where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).order(created_at: :desc)
     else
       render 'home'
     end
