9afc4304babbdec7b0052e605c095b9865183c83
Josiah Schaefer <penname.silas@gmail.com>
2015-07-28 10:30:19 -0400

    removed validation for url for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,6 @@ class Post < ActiveRecord::Base
 
   validates :title, presence: true, length: {minimum: 2}
   validates :description, presence: true
-  validates :url, presence: true
 
   sluggable_column :title
 end
