f11f20fb26dac6bbba1ec237e93bf80a7ed6c825
Josiah Schaefer <penname.silas@gmail.com>
2015-08-14 20:12:15 -0400

    nothing to see here

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_one :categories, through: :post_categories
 
   validates :title, uniqueness: true, presence: true, length: {minimum: 2}
   validates :description, presence: true
@@ -15,10 +15,4 @@ class Post < ActiveRecord::Base
   validates :categories, presence: true
 
   sluggable_column :title
-
-  
-
-  # def main_category
-  #   post.
-  # end
 end
