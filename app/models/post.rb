ea7c52223db7bc625a6cac64c4d35f8a127031dd
Josiah Schaefer <penname.silas@gmail.com>
2015-08-14 20:13:09 -0400

    going back to has many

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
-  has_one :categories, through: :post_categories
+  has_many :categories, through: :post_categories
 
   validates :title, uniqueness: true, presence: true, length: {minimum: 2}
   validates :description, presence: true
